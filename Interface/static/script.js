1e03b0be131fde1b9f8d112f05b844df2cf3da63
karimamez <karima.opensnz@gmail.com>
2023-03-09 11:25:26 +0100

    changes

--- Interface/static/script.js
+++ Interface/static/script.js
@@ -1,4 +1,4 @@
-function addDevice(){
+function addDevice() {
     console.log("add device");
 
     const appEUI = document.getElementById("AppEUI").value;
@@ -24,18 +24,18 @@ function addDevice(){
         return;
     }
 
-    
+
 
     const xhr = new XMLHttpRequest();
     xhr.open("POST", '/device/add', true);
-    
+
     //Send the proper header information along with the request
     xhr.setRequestHeader("Content-Type", "application/json");
-    
+
     xhr.onreadystatechange = () => { // Call a function when the state changes.
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             // Request finished. Do processing here.
-            
+
             console.log("Success")
             //location.href = "peripherique.html";
         }
@@ -45,61 +45,58 @@ function addDevice(){
         "AppKey": appKey,
         "DevEUI": devEUI
     }));
- 
-} 
+
+}
 
 
-function getDevices()
-{
+function getDevices() {
     console.log("get device")
 
     const xhr = new XMLHttpRequest();
     xhr.open("GET", '/device/all', true);
-    
-    
+
+
     xhr.onreadystatechange = () => { // Call a function when the state changes.
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             // Request finished. Do processing here.
-            
+
             console.log(JSON.parse(xhr.response))
         }
     }
-    xhr.send(); 
+    xhr.send();
 }
 
 
-function getSystem()
-{
+function getSystem() {
 
     const xhr = new XMLHttpRequest();
     xhr.open("GET", '/system', true);
-    
-    
+
+
     xhr.onreadystatechange = () => { // Call a function when the state changes.
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             // Request finished. Do processing here.
-            
+
             console.log(JSON.parse(xhr.response))
         }
     }
-    xhr.send(); 
+    xhr.send();
 }
 
 function generateRandomString(inputId, length) {
     var chars = '0123456789abcdef';
-    var result = '';    
-    var input = document.getElementById(inputId);  
+    var result = '';
+    var input = document.getElementById(inputId);
     for (var i = 0; i < length; i++) {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }  
+    }
     input.value = result;
 }
 function deleteDevices() {
     var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
     var DevEUIs = [];
 
-    checkboxes.forEach(function (checkbox)
- {
+    checkboxes.forEach(function (checkbox) {
         DevEUIs.push(checkbox.getAttribute("data-dev-eui"));
     });
 
@@ -133,31 +130,31 @@ function saveData() {
     var latitude = document.getElementById("latitude").value;
     var longitude = document.getElementById("longtitude").value;
     var altitude = document.getElementById("altitude").value;
-  
+
     var frequency = document.getElementById("frequency").value;
     var bandwidth = document.getElementById("SBW").value;
     var spreadingFactor = document.getElementById("spreadingfactor").value;
     var codingRate = document.getElementById("codingrate").value;
-  
+
     var data = {
-      radio_conf: {
-        frequency: frequency,
-        bandwidth: bandwidth,
-        spreading_factor: spreadingFactor,
-        coding_rate: codingRate,
-      },
-      gateway_conf: {
-        server_address: serverAddress,
-        server_port: serverPort,
-        gateway_id: gatewayId,
-        gateway_lat: latitude,
-        gateway_lon: longitude,
-        gateway_alt: altitude,
-      },
+        radio_conf: {
+            frequency: frequency,
+            bandwidth: bandwidth,
+            spreading_factor: spreadingFactor,
+            coding_rate: codingRate,
+        },
+        gateway_conf: {
+            server_address: serverAddress,
+            server_port: serverPort,
+            gateway_id: gatewayId,
+            gateway_lat: latitude,
+            gateway_lon: longitude,
+            gateway_alt: altitude,
+        },
     };
-  
+
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/network");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(data));
-  }
+}
